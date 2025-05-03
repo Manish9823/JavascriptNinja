299c5795966413cd784e061c1db864f2ed9e943e
Manish9823 <dmac77190@gmail.com>
2025-05-03 16:31:03 +0530

    Concepts calling generator from generator, parameter, exception

--- 6_1_Generator_Part1.js
+++ 6_1_Generator_Part1.js
@@ -80,3 +80,112 @@ for (const item of weaponIterator2) {
 // Kusarigama
 
 /// ============= END: Same above function iteration with for of loop ======
+
+
+/// ==============  START: Calling generator function from another generator function ===========
+
+// We can call generator function from another generator function by using yield* keyword
+// e.g
+
+function* getWarriors() {
+    yield "Sun Tzu";
+    yield* getNinja();
+    yield "Genghis Khan";
+    yield "Yoltas Waku";
+};
+
+function* getNinja() {
+    yield "Hattori";
+    yield "Yoshi";
+};
+
+for (const element of getWarriors()) {
+    console.log(element);
+}
+//output:
+// Sun Tzu
+// Hattori
+// Yoshi
+// Genghis Khan
+// Yoltas Waku
+
+/// ==============  END: Calling generator function from another generator function ===========
+
+
+/// ============== START: Generate unique id's using the generator ===============
+
+function* getUniqueId() {
+    let counter = 0;
+    while (true) {
+        yield ++counter;
+    }
+}
+
+const getUniqueIdIterator = getUniqueId();
+
+const firstId = getUniqueIdIterator.next().value;
+console.log({ firstId }); // {firstId: 1}
+const secondId = getUniqueIdIterator.next().value;
+console.log({ secondId }); // {secondId: 2}
+const thirdId = getUniqueIdIterator.next().value;
+console.log({ thirdId }); // {thirdId: 3}
+// ... and so on
+
+/// ============== END: Generate unique id's using the generator ===============
+
+
+
+/// ============== START: Sending Parameter in generator functions =============
+
+function* NinjaGenerator(action) {
+    // the yield will paused the execution by returning some value
+    // and when the iterator.next() method call then the further execution will start
+    // that means if the iterator.next("Test") send any parameter then it will get assign to the variable
+    // and here the variable is imposter
+    const imposter = yield ("Hattori " + action);
+    console.log(imposter === "Hanzo", " The generator has been infiltrated");
+
+    yield ("Yoshi (" + imposter + ") " + action);
+}
+
+const ninjaIterator = NinjaGenerator("skulk");
+
+// Here in the next method we are not passing any parameter
+// since the parameter will not assign to any variable and for initial parameter we passed the parameter in function call.
+// and after calling .next() method the execution will
+const result1 = ninjaIterator.next(); // {value: "Hattori skulk"}
+
+console.log(result1.value === "Hattori skulk", " Hattori is skulking.");
+
+const result2 = ninjaIterator.next("Hanzo"); // {value: "Yoshi (Hanzo) skulk"}
+console.log(result2.value === "Yoshi (Hanzo) skulk", " We have an imposter");
+
+//output
+// true  Hattori is skulking.
+// true  The generator has been infiltrated
+// true  We have an imposter
+
+/// ============== END: Sending Parameter in generator functions =============
+
+
+/// ============== Throwing Exception to generators
+
+function* NinjaGenerator1() {
+    try {
+        yield "Hattori";
+        console.log("The expected exception didn't occur");
+    } catch (error) {
+        console.log(error === "Catch this!", " Aha! we caught an exception.");
+    }
+}
+
+const ninjaIterator1 = NinjaGenerator1();
+
+const item1 = ninjaIterator1.next(); // { value: "Hattori"}
+console.log(item1.value === "Hattori", " We got Hattori.");
+
+ninjaIterator1.throw("Catch this!"); // Throws an exception to the generator
+
+// output
+// true  We got Hattori.
+// true  Aha! we caught an exception.
